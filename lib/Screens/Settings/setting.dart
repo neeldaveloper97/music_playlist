ba16b9291924728f708d091ad7d6a2e2e2a3c9ec
Neel dave <66321719+neeldaveloper97@users.noreply.github.com>
2022-12-26 16:06:25 +0530

    music application
    initial commit

---
+++ lib/Screens/Settings/setting.dart
@@ -0,0 +1,136 @@
+import 'dart:io';
+
+import 'package:blackhole/CustomWidgets/copy_clipboard.dart';
+import 'package:blackhole/CustomWidgets/gradient_containers.dart';
+import 'package:blackhole/CustomWidgets/popup.dart';
+import 'package:blackhole/CustomWidgets/snackbar.dart';
+import 'package:blackhole/CustomWidgets/textinput_dialog.dart';
+import 'package:blackhole/Helpers/backup_restore.dart';
+import 'package:blackhole/Helpers/config.dart';
+import 'package:blackhole/Helpers/countrycodes.dart';
+import 'package:blackhole/Helpers/picker.dart';
+import 'package:blackhole/Helpers/supabase.dart';
+import 'package:blackhole/Screens/Home/saavn.dart' as home_screen;
+import 'package:blackhole/Screens/Settings/player_gradient.dart';
+import 'package:blackhole/Screens/Top Charts/top.dart' as top_screen;
+import 'package:blackhole/Services/ext_storage_provider.dart';
+import 'package:blackhole/main.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class BoxSwitchTile extends StatelessWidget {
+  const BoxSwitchTile({
+    super.key,
+    required this.title,
+    this.subtitle,
+    required this.keyName,
+    required this.defaultValue,
+    this.isThreeLine,
+    this.onChanged,
+  });
+
+  final Text title;
+  final Text? subtitle;
+  final String keyName;
+  final bool defaultValue;
+  final bool? isThreeLine;
+  final Function(bool, Box box)? onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: Hive.box('settings').listenable(),
+      builder: (BuildContext context, Box box, Widget? widget) {
+        return SwitchListTile(
+          activeColor: Theme.of(context).colorScheme.secondary,
+          title: title,
+          subtitle: subtitle,
+          isThreeLine: isThreeLine ?? false,
+          dense: true,
+          value: box.get(keyName, defaultValue: defaultValue) as bool? ??
+              defaultValue,
+          onChanged: (val) {
+            box.put(keyName, val);
+            onChanged?.call(val, box);
+          },
+        );
+      },
+    );
+  }
+}
+
+class SpotifyCountry {
+  Future<String> changeCountry({required BuildContext context}) async {
+    String region =
+        Hive.box('settings').get('region', defaultValue: 'India') as String;
+    if (!ConstantCodes.localChartCodes.containsKey(region)) {
+      region = 'India';
+    }
+
+    await showModalBottomSheet(
+      isDismissible: true,
+      backgroundColor: Colors.transparent,
+      context: context,
+      builder: (BuildContext context) {
+        const Map<String, String> codes = ConstantCodes.localChartCodes;
+        final List<String> countries = codes.keys.toList();
+        return BottomGradientContainer(
+          borderRadius: BorderRadius.circular(
+            20.0,
+          ),
+          child: ListView.builder(
+            physics: const BouncingScrollPhysics(),
+            shrinkWrap: true,
+            padding: const EdgeInsets.fromLTRB(
+              0,
+              10,
+              0,
+              10,
+            ),
+            itemCount: countries.length,
+            itemBuilder: (context, idx) {
+              return ListTileTheme(
+                selectedColor: Theme.of(context).colorScheme.secondary,
+                child: ListTile(
+                  title: Text(
+                    countries[idx],
+                  ),
+                  leading: Radio(
+                    value: countries[idx],
+                    groupValue: region,
+                    onChanged: (value) {
+                      top_screen.localSongs = [];
+                      region = countries[idx];
+                      top_screen.fetched = false;
+                      top_screen.fetchFinished.value = false;
+                      Hive.box('settings').put('region', region);
+                      Navigator.pop(context);
+                    },
+                  ),
+                  selected: region == countries[idx],
+                  onTap: () {
+                    top_screen.localSongs = [];
+                    region = countries[idx];
+                    top_screen.fetched = false;
+                    top_screen.fetchFinished.value = false;
+                    Hive.box('settings').put('region', region);
+                    Navigator.pop(context);
+                  },
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+    return region;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,230 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:audio_service/audio_service.dart';
+import 'package:blackhole/Helpers/config.dart';
+import 'package:blackhole/Helpers/countrycodes.dart';
+import 'package:blackhole/Helpers/handle_native.dart';
+import 'package:blackhole/Helpers/route_handler.dart';
+import 'package:blackhole/Screens/Home/home.dart';
+import 'package:blackhole/Screens/Library/downloads.dart';
+import 'package:blackhole/Screens/Library/nowplaying.dart';
+import 'package:blackhole/Screens/Library/playlists.dart';
+import 'package:blackhole/Screens/Library/recent.dart';
+import 'package:blackhole/Screens/Login/auth.dart';
+import 'package:blackhole/Screens/Login/pref.dart';
+import 'package:blackhole/Screens/Player/audioplayer.dart';
+import 'package:blackhole/Services/audio_service.dart';
+import 'package:blackhole/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Paint.enableDithering = true;
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    await Hive.initFlutter('BlackHole');
+  } else {
+    await Hive.initFlutter();
+  }
+  await openHiveBox('settings');
+  await openHiveBox('downloads');
+  await openHiveBox('Favorite Songs');
+  await openHiveBox('cache', limit: true);
+  if (Platform.isAndroid) {
+    setOptimalDisplayMode();
+  }
+  await startService();
+  runApp(MyApp());
+}
+
+Future<void> setOptimalDisplayMode() async {
+  final List<DisplayMode> supported = await FlutterDisplayMode.supported;
+  final DisplayMode active = await FlutterDisplayMode.active;
+
+  final List<DisplayMode> sameResolution = supported
+      .where(
+        (DisplayMode m) => m.width == active.width && m.height == active.height,
+      )
+      .toList()
+    ..sort(
+      (DisplayMode a, DisplayMode b) => b.refreshRate.compareTo(a.refreshRate),
+    );
+
+  final DisplayMode mostOptimalMode =
+      sameResolution.isNotEmpty ? sameResolution.first : active;
+
+  await FlutterDisplayMode.setPreferredMode(mostOptimalMode);
+}
+
+Future<void> startService() async {
+  final AudioPlayerHandler audioHandler = await AudioService.init(
+    builder: () => AudioPlayerHandlerImpl(),
+    config: AudioServiceConfig(
+      androidNotificationChannelId: 'com.shadow.blackhole.channel.audio',
+      androidNotificationChannelName: 'BlackHole',
+      androidNotificationOngoing: true,
+      androidNotificationIcon: 'drawable/ic_stat_music_note',
+      androidShowNotificationBadge: true,
+      // androidStopForegroundOnPause: Hive.box('settings')
+      // .get('stopServiceOnPause', defaultValue: true) as bool,
+      notificationColor: Colors.grey[900],
+    ),
+  );
+  GetIt.I.registerSingleton<AudioPlayerHandler>(audioHandler);
+  GetIt.I.registerSingleton<MyTheme>(MyTheme());
+}
+
+Future<void> openHiveBox(String boxName, {bool limit = false}) async {
+  final box = await Hive.openBox(boxName).onError((error, stackTrace) async {
+    final Directory dir = await getApplicationDocumentsDirectory();
+    final String dirPath = dir.path;
+    File dbFile = File('$dirPath/$boxName.hive');
+    File lockFile = File('$dirPath/$boxName.lock');
+    if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+      dbFile = File('$dirPath/BlackHole/$boxName.hive');
+      lockFile = File('$dirPath/BlackHole/$boxName.lock');
+    }
+    await dbFile.delete();
+    await lockFile.delete();
+    await Hive.openBox(boxName);
+    throw 'Failed to open $boxName Box\nError: $error';
+  });
+  // clear box if it grows large
+  if (limit && box.length > 500) {
+    box.clear();
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale _locale = const Locale('en', '');
+  late StreamSubscription _intentDataStreamSubscription;
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  void dispose() {
+    _intentDataStreamSubscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    final String systemLangCode = Platform.localeName.substring(0, 2);
+    if (ConstantCodes.languageCodes.values.contains(systemLangCode)) {
+      _locale = Locale(systemLangCode);
+    } else {
+      final String lang =
+          Hive.box('settings').get('lang', defaultValue: 'English') as String;
+      _locale = Locale(ConstantCodes.languageCodes[lang] ?? 'en');
+    }
+
+    AppTheme.currentTheme.addListener(() {
+      setState(() {});
+    });
+
+    // For sharing or opening urls/text coming from outside the app while the app is in the memory
+    _intentDataStreamSubscription = ReceiveSharingIntent.getTextStream().listen(
+      (String value) {
+        handleSharedText(value, navigatorKey);
+      },
+      onError: (err) {
+        // print("ERROR in getTextStream: $err");
+      },
+    );
+
+    // For sharing or opening urls/text coming from outside the app while the app is closed
+    ReceiveSharingIntent.getInitialText().then(
+      (String? value) {
+        if (value != null) handleSharedText(value, navigatorKey);
+      },
+    );
+  }
+
+  void setLocale(Locale value) {
+    setState(() {
+      _locale = value;
+    });
+  }
+
+  Widget initialFuntion() {
+    return Hive.box('settings').get('userId') != null
+        ? HomePage()
+        : AuthScreen();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: AppTheme.themeMode == ThemeMode.dark
+            ? Colors.black38
+            : Colors.white,
+        statusBarIconBrightness: AppTheme.themeMode == ThemeMode.dark
+            ? Brightness.light
+            : Brightness.dark,
+        systemNavigationBarIconBrightness: AppTheme.themeMode == ThemeMode.dark
+            ? Brightness.light
+            : Brightness.dark,
+      ),
+    );
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+
+    return MaterialApp(
+      title: 'Simple Music',
+      restorationScopeId: 'simpleMusic',
+      debugShowCheckedModeBanner: false,
+      themeMode: AppTheme.themeMode,
+      theme: AppTheme.lightTheme(
+        context: context,
+      ),
+      darkTheme: AppTheme.darkTheme(
+        context: context,
+      ),
+      locale: _locale,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: ConstantCodes.languageCodes.entries
+          .map((languageCode) => Locale(languageCode.value, ''))
+          .toList(),
+      routes: {
+        '/': (context) => initialFuntion(),
+        '/pref': (context) => const PrefScreen(),
+        '/playlists': (context) => PlaylistScreen(),
+        '/nowplaying': (context) => NowPlaying(),
+        '/recent': (context) => RecentlyPlayed(),
+        '/downloads': (context) => const Downloads(),
+      },
+      navigatorKey: navigatorKey,
+      onGenerateRoute: (RouteSettings settings) {
+        return HandleRoute.handleRoute(settings.name);
+      },
+    );
+  }
+}
